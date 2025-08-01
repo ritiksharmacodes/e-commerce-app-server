467eeacd8060c1189bdf873e2759289af7412f35
ritiksharmacodes <ritikcan18@gmail.com>
2025-08-01 17:20:20 +0530

    fixed the auth bug

--- app.js
+++ app.js
@@ -2,23 +2,22 @@ import "dotenv/config";
 import express from "express";
 import morgan from "morgan";
 import cors from 'cors';
-import helmet from "helmet";
-
 
 import productsRouter from "./routes/products.js";
 import { router as authRouter } from "./routes/auth.js";
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',   // your React dev server
+    credentials: true                 // allow cookies and Authorization headers
+}));
 app.use(morgan('common'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({
     extended: true,
     limit: '50mb'
 }));
-app.use(cors());
-app.use(helmet());
-
 
 const currentApiPath = '/api/v1/';
 
--- routes/auth.js
+++ routes/auth.js
@@ -5,23 +5,31 @@ import jwt from "jsonwebtoken";
 
 const router = new Router();
 
-router.post('/signup', async (request, response) => {    
+router.post('/signup', async (request, response) => {
     try {
         const email = request.body.email;
         const username = request.body.username;
         const pwd = request.body.password;
 
         const hashedPwd = await hash(pwd, 10);
-        const res = await query('INSERT INTO users(username, email, password, auth_provider) VALUES ($1, $2, $3, $4)',
-            [username, email, hashedPwd, 'local']
+        const res = await query("INSERT INTO users(uuid, username, email, password, auth_provider) VALUES (uuid_generate_v4(), $1, $2, $3, 'local')",
+            [username, email, hashedPwd]
         );
 
         const token = jwt.sign({ "email": email, "username": username }, process.env.JWT_PRIVATE_KEY);
-        response.cookie("user", token);
+
+        return response
+            .cookie("user", token, {
+                httpOnly: true,
+                secure: true,         // Needed for HTTPS
+                sameSite: 'None'      // Needed for cross-origin 
+            })
+            .status(201)
+            .json({ message: "Signup successful", token });
 
     } catch (error) {
         console.log(error);
-        response.json(error);
+        return response.json(error);
     }
 });
 
